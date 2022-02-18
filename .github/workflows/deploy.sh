07202288dd1104fc3dca5addbafa0a5c59a03b2a
thanhdx <thanhdx@hybrid-technologies.co.jp>
2022-02-18 15:18:36 +0700

    --amend

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:thanhinterpol/devops_demo.git main:gh-pages
+git push -f git@github.com:thanhinterpol/vuejs_demo.git main:gh-pages
 
 cd -
