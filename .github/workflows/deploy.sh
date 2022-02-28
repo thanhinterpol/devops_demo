15544f0ab0c51e0bb6b9f4b48a9055b4f5177416
thanhdx <thanhdx@hybrid-technologies.co.jp>
2022-02-28 14:02:38 +0700

    Test

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -7,18 +7,18 @@ set -e
 npm run build
 
 # navigate into the build output directory
-cd dist
+# cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
-git add -A
+# git init
+git add dist/*
 git commit -m 'deploy'
-git branch -M main
-echo "Thanh 1"
-git remote add origin "https://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@github.com/thanhinterpol/vuejs_demo.git"
-git remote -v
+# git branch -M main
+# echo "Thanh 1"
+# git remote add origin "https://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@github.com/thanhinterpol/vuejs_demo.git"
+# git remote -v
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:thanhinterpol/thanhinterpol.github.io.git main
